6880e6305a361e3d0e30c3f6a90f24cdecf3279c
Gustavo Spiess <gustavospiess@gmail.com>
2016-10-26 19:34:42 -0200

    doc-update

--- referencias/AutoComplete/What is autocomplete_files/ca-pub-3999293766533555.js
+++
@@ -1,2 +0,0 @@
-(google_async_config = window.google_async_config || {})['ca-pub-3999293766533555'] = {"sra_enabled":true};
-try{window.localStorage.setItem('google_sra_enabled', '1');}catch(e){}
--- referencias/AutoComplete/What is autocomplete_files/recent.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("ra1").innerHTML = "<a href=\"http://www.computerhope.com/issues/ch001757.htm\">How to show or hide desktop shortcut icons.</a>";
-document.getElementById("ra2").innerHTML = "<a href=\"http://www.computerhope.com/comp/lian_li.htm\">Lian li company and contact information.</a>";
-document.getElementById("ra3").innerHTML = "<a href=\"http://www.computerhope.com/issues/ch001730.htm\">How to create an audio file.</a>";
-document.getElementById("ra4").innerHTML = "<a href=\"http://www.computerhope.com/issues/ch001731.htm\">How to move a window.</a>";
-document.getElementById("ra5").innerHTML = "<a href=\"http://www.computerhope.com/issues/ch001732.htm\">How are computers used?</a>";
